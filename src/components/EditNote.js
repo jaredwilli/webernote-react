663456850dbd4e574bd508d089c841041560d4e4
jaredwilli <jaredwilli@gmail.com>
2017-11-17 14:55:26 -0500

    fix issues

--- src/components/EditNote.js
+++ src/components/EditNote.js
@@ -33,8 +33,8 @@ class EditNote extends React.Component {
         const { selectedNote } = nextProps;
 
         if (selectedNote) {
-            this.setState(notebook => {
-                notebook: (selectedNote.notebook && Object.keys(selectedNote.notebook).length) ?
+            this.setState({
+                notebook: (selectedNote.notebook && Object.keys(selectedNote.notebook).length) ? selectedNote.notebook : this.state.notebook
             });
         }
     }
@@ -44,9 +44,7 @@ class EditNote extends React.Component {
 
         selectedNote[e.target.name] = e.target.value;
 
-        this.setState({
-            selectedNote
-        });
+        this.setState({ selectedNote });
 
         this.props.actions.editNote(selectedNote);
         this.props.actions.getNotes();
