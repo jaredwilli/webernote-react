d3ba61a6e50fefab99e7c5c60a0c04c39e250ba5
jaredwilli <jaredwilli@gmail.com>
2017-11-20 20:43:21 -0500

    fix things and create things

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,18 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { createLogger } from 'redux-logger';
+// import { createLogger } from 'redux-logger';
 import rootReducer from '../reducers/rootReducer';
 
-const logger = createLogger();
+// const logger = createLogger();
 const initialState = {};
 
 // Set up Redux DevTools
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-debugger;
+
 const configureStore = createStore(
     rootReducer,
     initialState,
-    composeEnhancers(applyMiddleware(thunk, logger))
+    composeEnhancers(applyMiddleware(thunk))
 );
 
 export default configureStore;
