b8eba82e8247f8b38ca09b5853f95b0f14e3a5fa
jaredwilli <jaredwilli@gmail.com>
2017-11-21 23:54:17 -0500

    almost got react drag and drop working for note-list column. kinda not sure its really worth it though...

--- src/components/Note.js
+++ src/components/Note.js
@@ -7,12 +7,30 @@ import TagsList from './TagsList';
 import { formatDate, shorten } from '../common/helpers';
 import { sortNotes } from '../common/noteHelpers';
 
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+// using some little inline style helpers to make the app look okay
+const grid = 8;
+export const getItemStyle = (draggableStyle, isDragging) => ({
+	// some basic styles to make the notes look a bit nicer
+	padding: grid * 2,
+	margin: `0 0 ${grid}px 0`,
+	background: isDragging ? 'lightgreen' : 'white',
+	...draggableStyle
+});
+
+export const getListStyle = isDraggingOver => ({
+	background: isDraggingOver ? 'lightblue' : 'lightgrey',
+	padding: grid,
+	width: '100%'
+});
+
 const Note = ({ notes = [], isMobile = false, ...props }) => {
     if (!notes.length) {
         return <div className="empty"></div>;
     }
 
-    const note = sortNotes(notes).map(note =>
+    /* const note = sortNotes(notes).map(note =>
         <li className={(note.isEditing) ? 'note selected' : 'note'}
             key={note.id} id={note.id}
             onClick={(e) => props.selectNote(note)}>
@@ -48,9 +66,72 @@ const Note = ({ notes = [], isMobile = false, ...props }) => {
                 </div>
             </div>
         </li>
-    );
+    ); */
 
-    return <div className="notes"><ul>{note}</ul></div>;
+    const noteItems = notes.map(note => (
+        <Draggable key={note.id} draggableId={note.id}>
+            {(provided, snapshot) => (
+                <div>
+                    <div ref={provided.innerRef}
+                        style={getItemStyle(provided.draggableStyle, snapshot.isDragging)}
+                        {...provided.dragHandleProps}>
+
+                        {!isMobile && <CloseBtn onClick={(e) => props.deleteNote(note)} />}
+                        {(isMobile && note.url) && <a className="external-link" href={note.url} target="_blank"><FontAwesome name='external-link' /></a>}
+
+                        {note.label && <div className="note-label" style={{ background: note.label.hex }}></div>}
+
+                        {note.title && <h2 className="title">
+                            {(!isMobile && note.url) &&
+                                <a href={note.url} target="_blank">{shorten(note.title, 80)}</a>
+                            }
+                            {(!isMobile && !note.url) && shorten(note.title, 80)}
+                            {(isMobile) && shorten(note.title, 80)}
+                        </h2>}
+
+                        <div className="note-details">
+                            <span className="date">
+                                {formatDate((note.modified_date) ? note.modified_date : note.created_date)}
+                            </span>
+
+                            <span className="description">{shorten(note.description, 250)}</span>
+
+                            <div className="taxonomies">
+                                {(note.notebook && note.notebook.name) &&
+                                    <span className="notebook">
+                                        <FontAwesome name='book' />
+                                        <a href>{note.notebook.name}</a>
+                                    </span>
+                                }
+                                <TagsList tags={note.tags} />
+                            </div>
+                        </div>
+                    </div>
+
+                    {provided.placeholder}
+                </div>
+            )}
+        </Draggable>
+    ));
+
+    return (
+        <div className="notes">
+            <DragDropContext onDragEnd={props.onDragEnd}>
+                <Droppable droppableId="droppable">
+                    {(provided, snapshot) => (
+                        <div
+                            ref={provided.innerRef}
+                            style={getListStyle(snapshot.isDraggingOver)}>
+
+                            {noteItems}
+
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
+            </DragDropContext>
+        </div>
+    );
 };
 
 export default Note;
--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -10,153 +11,197 @@ import Note from './Note';
 
 import * as noteActions from '../actions/noteActions';
 
+// a little function to help us with reordering the result
+export const reorder = (list, startIndex, endIndex) => {
+	const result = Array.from(list);
+	const [removed] = result.splice(startIndex, 1);
+	result.splice(endIndex, 0, removed);
+
+	return result;
+};
+
 class NoteList extends React.Component {
-    constructor(props, context) {
-        super(props, context);
+	constructor(props, context) {
+		super(props, context);
 
-        this.selectNote = this.selectNote.bind(this);
-        this.deleteNote = this.deleteNote.bind(this);
-        this.filterNotes = this.filterNotes.bind(this);
-        this.clearFilters = this.clearFilters.bind(this);
+		this.selectNote = this.selectNote.bind(this);
+		this.deleteNote = this.deleteNote.bind(this);
+		this.filterNotes = this.filterNotes.bind(this);
+		this.clearFilters = this.clearFilters.bind(this);
 
-        this.state = {
-            windowWidth: window.innerWidth,
-            filterType: 'Title',
-            searchTerm: '',
+		this.onDragEnd = this.onDragEnd.bind(this);
+
+		this.state = {
+			notes: this.props.notes,
+			windowWidth: window.innerWidth,
+			filterType: 'Title',
+			searchTerm: '',
 			notebookFilter: {
 				name: 'All notebooks',
 				id: 'all_notebooks'
-            },
-            sort: {
-                order: 'desc',
-                sortBy: 'created_date'
-            }
-        };
-    }
+			},
+			sort: {
+				order: 'desc',
+				sortBy: 'created_date'
+			}
+		};
+	}
 
-    componentDidMount() {
-        this.onWindowResize();
-        window.addEventListener('resize', this.onWindowResize);
-    }
+	componentDidMount() {
+		this.onWindowResize();
+		window.addEventListener('resize', this.onWindowResize);
+	}
 
-    // make sure to remove the listener
-    // when the component is not mounted anymore
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.onWindowResize);
-    }
+	// make sure to remove the listener
+	// when the component is not mounted anymore
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.onWindowResize);
+	}
 
-    onWindowResize = () => {
-        this.setState({ width: window.innerWidth });
-    };
+	onWindowResize = () => {
+		this.setState({ width: window.innerWidth });
+	};
 
-    clearFilters() {
-        this.setState({
-            filterType: 'Title',
-            searchTerm: '',
-            notebookFilter: {
-                name: 'All notebooks',
-                id: 'all_notebooks'
-            }
-        }, () => this.props.filterNotes());
-    }
+	clearFilters() {
+		this.setState(
+			{
+				filterType: 'Title',
+				searchTerm: '',
+				notebookFilter: {
+					name: 'All notebooks',
+					id: 'all_notebooks'
+				}
+			},
+			() => this.props.filterNotes()
+		);
+	}
 
-    filterNotes(filter) {
-        if (filter) {
-            this.setState(filter, () => {
-                let filterVals = {};
-                const { notebookFilter, filterType, searchTerm } = this.state;
+	filterNotes(filter) {
+		if (filter) {
+			this.setState(filter, () => {
+				let filterVals = {};
+				const { notebookFilter, filterType, searchTerm } = this.state;
 
-                if (filterType && searchTerm) {
-                    filterVals.type = filterType;
-                    filterVals.term = searchTerm;
-                } else if (notebookFilter) {
-                    filterVals.notebook = notebookFilter;
-                }
+				if (filterType && searchTerm) {
+					filterVals.type = filterType;
+					filterVals.term = searchTerm;
+				} else if (notebookFilter) {
+					filterVals.notebook = notebookFilter;
+				}
 
-                this.props.filterNotes(filterVals);
-            });
-        } else {
-            this.clearFilters();
-        }
-    }
+				this.props.filterNotes(filterVals);
+			});
+		} else {
+			this.clearFilters();
+		}
+	}
 
-    selectNote(note) {
-        this.props.actions.resetSelectedNote();
-        this.props.actions.selectNote(note);
-    }
+	onDragEnd(result) {
+		// dropped outside the list
+		if (!result.destination) {
+			return;
+		}
 
-    deleteNote(note) {
-        this.props.deleteNote(note);
-    }
+		const notes = reorder(
+			this.props.notes,
+			result.source.index,
+			result.destination.index
+		);
 
-    render() {
-        const { notes, notebooks = [] } = this.props;
-        const { filterType, searchTerm, notebookFilter, width } = this.state;
-        const isMobile = width <= 690;
+		this.setState({
+			notes
+		});
+	}
 
-        // Handle when theres no notes
-        if (!notes.length) {
-            if (searchTerm.length === 0 && notebookFilter.id === 'all_notebooks') {
-                return (
-                    <WelcomeMsg
-                        addNote={this.props.addNote}
-                        showLoginModal={this.props.showLoginModal} />
-                );
-            }
-        }
+	selectNote(note) {
+		this.props.actions.resetSelectedNote();
+		this.props.actions.selectNote(note);
+	}
 
-        return (
-            <div className="middle list-col note-list">
-                <div className="filters">
-                    <div className="filter">
-                        <SearchFilter
-                            notes={notes}
-                            filterType={filterType}
-                            searchTerm={searchTerm}
-                            onChange={this.filterNotes}
-                            clearFilters={this.clearFilters} />
-                    </div>
+	deleteNote(note) {
+		this.props.deleteNote(note);
+	}
 
-                    {(notebooks.length > 0) &&
-                        <div className="viewing">
-                            <ViewCount
-                                notes={notes}
-                                notebooks={notebooks}
-                                notebookFilter={notebookFilter}
-                                onChange={this.filterNotes} />
-                        </div>
-                    }
-                </div>
+	render() {
+		const { notes, notebooks = [] } = this.props;
+		const { filterType, searchTerm, notebookFilter, width } = this.state;
+		const isMobile = width <= 690;
 
-                {!notes.length && <div className="empty">No notes to show...</div>}
+		// Handle when theres no notes
+		if (!notes.length) {
+			if (
+				searchTerm.length === 0 &&
+				notebookFilter.id === 'all_notebooks'
+			) {
+				return (
+					<WelcomeMsg
+						addNote={this.props.addNote}
+						showLoginModal={this.props.showLoginModal}
+					/>
+				);
+			}
+		}
 
-                {notes.length &&
-                    <Note
-                        notes={notes}
-                        sort={this.state.sort}
-                        selectNote={(note) => this.selectNote(note)}
-                        deleteNote={(note) => this.deleteNote(note)}
-                        isMobile={isMobile} />
-                }
-            </div>
-        );
-    }
+		return (
+			<div className="middle list-col note-list">
+				<div className="filters">
+					<div className="filter">
+						<SearchFilter
+							notes={notes}
+							filterType={filterType}
+							searchTerm={searchTerm}
+							onChange={this.filterNotes}
+							clearFilters={this.clearFilters}
+						/>
+					</div>
+
+					{notebooks.length > 0 && (
+						<div className="viewing">
+							<ViewCount
+								notes={notes}
+								notebooks={notebooks}
+								notebookFilter={notebookFilter}
+								onChange={this.filterNotes}
+							/>
+						</div>
+					)}
+				</div>
+
+				{!notes.length && (
+					<div className="empty">No notes to show...</div>
+				)}
+
+				{notes.length && (
+					<Note
+						notes={notes}
+						sort={this.state.sort}
+						selectNote={note => this.selectNote(note)}
+						deleteNote={note => this.deleteNote(note)}
+						isMobile={isMobile}
+                        onDragEnd={this.onDragEnd}
+					/>
+				)}
+			</div>
+		);
+	}
 }
 
 function mapStateToProps(state) {
-    const newState = {
-        notebooks: state.notebookData.notebooks,
-        tags: state.tagData.tags
-    };
-    // console.log('STATE: ', state, newState);
+	const newState = {
+		notebooks: state.notebookData.notebooks,
+		tags: state.tagData.tags
+	};
+	// console.log('STATE: ', state, newState);
 
-    return newState;
+	return newState;
 }
 
 function mapDispatchToProps(dispatch) {
-    return {
-        actions: bindActionCreators(noteActions, dispatch)
-    };
+	return {
+		actions: bindActionCreators(noteActions, dispatch)
+	};
 }
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(NoteList));
+export default withRouter(
+	connect(mapStateToProps, mapDispatchToProps)(NoteList)
+);
